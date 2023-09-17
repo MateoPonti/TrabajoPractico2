1034e550df846b252d75057467350069e2888457
MateoPonti <63692136+MateoPonti@users.noreply.github.com>
2023-09-17 16:13:41 -0300

    Inicio ej1

--- src/unlu/poo/clubFutbol/Club.java
+++ src/unlu/poo/clubFutbol/Club.java
@@ -1,6 +1,11 @@
 package unlu.poo.clubFutbol;
 
+import javax.swing.text.DateFormatter;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 
 public class Club {
     ArrayList<Socio> listaDeSocios;
@@ -10,8 +15,15 @@ public class Club {
         listaDeSocios= new ArrayList<>();
     }
 
-    public void cargarSocio(String nombre,Tipo suscrip){
-        Socio socio= new Socio(nombre,new Suscripcion(suscrip));
+    public boolean cargarSocio(String nombre,Tipo suscrip, int tel, String correo,String dia){
+        try    {
+            LocalDate fecha=LocalDate.parse(dia,DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+            Socio socio= new Socio(nombre,new Suscripcion(suscrip),tel,correo,fecha);
+            return true;
+        }
+        catch (DateTimeParseException  e){
+            return false;
+        }
     }
 
     public String devolverSocios(){
@@ -22,4 +34,34 @@ public class Club {
         return listaSocios.toString();
     }
 
-}
+
+    public String devolverSociosPorSuscripcion(){
+        StringBuilder listaSociosPorSuscripcion= new StringBuilder();
+        listaSociosPorSuscripcion.append(cargarValor(Tipo.BASICA)).append(cargarValor(Tipo.INTERMEDIA)).append(cargarValor(Tipo.DESTACADA));
+        return listaSociosPorSuscripcion.toString();
+    }
+
+
+    private String cargarValor(Tipo tipo){
+        StringBuilder listaSociosPorSuscripcion= new StringBuilder();
+        listaSociosPorSuscripcion.append("Suscriptores de la suscripcion "+tipo.toString());
+        for (Socio socio:listaDeSocios){
+        if (socio.getSuscripcion().comparar(tipo)){listaSociosPorSuscripcion.append("-").append(socio).append("\n");}}
+        return listaSociosPorSuscripcion.toString();
+    }
+
+    private  int buscarSocio(int id){
+    int pos=0;
+        for (Socio socio:listaDeSocios){
+            if (socio.compararId(id)){
+                return pos;
+            }
+            pos++;
+        }
+     return -1;
+    }
+
+
+
+
+    }
--- src/unlu/poo/clubFutbol/Socio.java
+++ src/unlu/poo/clubFutbol/Socio.java
@@ -1,20 +1,78 @@
 package unlu.poo.clubFutbol;
 
+import java.time.LocalDate;
+
 public class Socio {
     private Suscripcion suscripcion;
     private String nombre;
 
-    public Socio(String nombre,Suscripcion suscripcion) {
+    private  int telefono;
+
+
+    private LocalDate fechaInscripcion;
+
+
+
+    private String correo;
+
+    private int id;
+
+    private static int numerosId=0;
+
+
+
+    public Socio(String nombre, Suscripcion suscripcion, int tel, String correo, LocalDate fecha) {
         this.nombre=nombre;
         this.suscripcion=suscripcion;
+        this.correo=correo;
+        this.telefono=tel;
+        this.fechaInscripcion=fecha;
+        numerosId++;
+        id=numerosId;
+
     }
 
     @Override
     public String toString() {
-        if (suscripcion==null){return this.nombre+" , Suscripcion : Nula";}
-        else{return this.nombre+" , Suscripcion : "+suscripcion.toString();}
+      return "id: "+id+", nombre: "+ getNombre()+" , Suscripcion : "+suscripcion.toString()+ " , correo : "+getCorreo()+ ", telefono: "+getTelefono();}
+
+
+    public void setSuscripcion(Suscripcion suscripcion){
+     this.suscripcion=suscripcion;
+    }
+    public Suscripcion getSuscripcion() {
+        return suscripcion;
     }
 
-    public asignarSuscripcion()
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public boolean compararId(int numero) {
+        return id==numero;
+    }
+
+
+
 
 }
